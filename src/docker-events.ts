781225364031fcda29a3db8ccc0a1e00f35994ea
naimo84 <git@naimo84.dev>
2021-10-27 19:48:36 +0200

    feat: add recconnect for docker events #17

--- src/docker-events.ts
+++ src/docker-events.ts
@@ -8,51 +8,89 @@ export interface DockerEvent {
     timeNano?: string
 }
 
-module.exports = function (RED: Red) {  
+module.exports = function (RED: Red) {
 
-    function DockerEvents(n:any) {
-        let node:any = this as any;
+    function DockerEvents(n: any) {
+        let node: any = this as any;
         RED.nodes.createNode(node, n);
 
         let config = (RED.nodes.getNode(n.config) as unknown as DockerConfiguration);
         let client = config.getClient();
+        let globalEvents = null;
 
-        client.getEvents().then((events:any) => {          
-          
-            node.status({ fill: 'green', shape: 'dot', text: 'node-red:common.status.connected' });
-
-            events.on('data', (data) => {
-                let event: DockerEvent = {};
-                try {
-                    event = JSON.parse(data.toString());
-                } catch (e) {
-                    node.error('Error parsing JSON', e);
-                    return
+        node.on('input', async (_msg, send, done) => {
+            try {
+                if(globalEvents !== null){
+                    globalEvents.removeAllListeners();
+                    globalEvents.destroy();
                 }
 
-                node.send({
-                    _msgid: RED.util.generateId(),
-                    type: event.Type,
-                    action: event.Action,
-                    time: event.time,
-                    timeNano: event.timeNano,
-                    payload: event
-                });
-            });
+                const events: NodeJS.ReadableStream = await client.getEvents()
+                globalEvents = events;
+                node.status({ fill: 'green', shape: 'dot', text: 'node-red:common.status.connected' });
 
-            events.on('close', () => {
-                node.status({ fill: 'red', shape: 'ring', text: 'node-red:common.status.disconnected' });
-                node.warn('Docker event stream closed.');
-            });
-            events.on('error', (err) => {
-                node.status({ fill: 'red', shape: 'ring', text: 'node-red:common.status.disconnected' });
-                node.error('Error:', err);
-            });
-            events.on('end', () => {
-                node.status({ fill: 'yellow', shape: 'ring', text: 'stream ended' });
-                node.warn('Docker event stream ended.');
-            });
-        });
+                events.on('data', (data) => {
+                    let event: DockerEvent = {};
+                    try {
+                        event = JSON.parse(data.toString());
+                    } catch (e) {
+                        node.error('Error parsing JSON', e);
+                        return
+                    }
+
+                    send({
+                        _msgid: RED.util.generateId(),
+                        type: event.Type,
+                        action: event.Action,
+                        time: event.time,
+                        timeNano: event.timeNano,
+                        payload: event
+                    });
+                    done();
+                });
+
+                events.on('close', () => {
+                    node.status({ fill: 'red', shape: 'ring', text: 'node-red:common.status.disconnected' });
+                    node.warn('Docker event stream closed.');
+                    send([null, {
+                        _msgid: RED.util.generateId(),
+                        type: 'dockerode',
+                        payload: {
+                            connected: false
+                        }
+                    }])
+                    done();
+                });
+                events.on('error', (err) => {
+                    node.status({ fill: 'red', shape: 'ring', text: 'node-red:common.status.disconnected' });
+                    send([null, {
+                        _msgid: RED.util.generateId(),
+                        type: 'dockerode',
+                        payload: {
+                            connected: false
+                        }
+                    }])
+                    done('Error:', err);
+                });
+                events.on('end', () => {
+                    node.status({ fill: 'yellow', shape: 'ring', text: 'stream ended' });
+                    node.warn('Docker event stream ended.');
+                    done();
+                });
+            }
+            catch {
+                console.error('Failed to attach docker event listener.');
+                send([null, {
+                    _msgid: RED.util.generateId(),
+                    type: 'dockerode',
+                    payload: {
+                        connected: false
+                    }
+                }])
+                done();
+            }
+        })
+        node.emit("input", {});
     }
 
     RED.nodes.registerType('docker-events', DockerEvents);
